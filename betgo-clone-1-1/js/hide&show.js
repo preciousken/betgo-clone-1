3f5f85aea1618dc723227ab6a0c3da0a1e5b2f7d
preciousken <114781822+preciousken@users.noreply.github.com>
2022-10-25 14:13:18 -1100

    Add files via upload
    this is the second improved design for betgo clone. still working on the designs

---
+++ betgo-clone-1-1/js/hide&show.js
@@ -0,0 +1,4 @@
+console.log('hide&show')
+const closeSidebar = document.querySelector('.sidebar .sidebar-fluid div .closeSidebar')
+
+closeSidebar.addEventListener('click',()=>console.log('canceled'))
